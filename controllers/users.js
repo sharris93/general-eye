1591762195c1d187960fd0b42b04f473ce35fae6
Sam <sharrisdev@gmail.com>
2025-04-16 16:07:31 +0100

    added profile page with authored and liked articles

--- controllers/users.js
+++ controllers/users.js
@@ -1,12 +1,22 @@
 import express from 'express'
 import User from '../models/User.js'
+import Article from '../models/Article.js'
 
 const router = express.Router()
 
 router.get('/profile', async (req, res, next) => {
   try {
-    console.log(req.session.user)
-    return res.render('users/profile.ejs')
+    // Get all authored articles
+    const authoredArticles = await Article.find({ author: req.session.user._id })
+
+    // Get all liked articles
+    const likedArticles = await Article.find({ likes: req.session.user._id })
+
+    // Render
+    return res.render('users/profile.ejs', {
+      authoredArticles,
+      likedArticles
+    })
   } catch (error) {
     console.log(error)
   }
